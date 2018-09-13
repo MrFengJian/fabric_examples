69390ad8ed304f9bc544c0933cf2beeef22c4ab2
fengjj <fengjj@7zhilu.com>
2018-09-13 11:28:40 +0800

    update .gitignore

--- cc_example/main.go
+++ cc_example/main.go
@@ -1,1 +1,29 @@
-package cc_example
+package main
+
+import (
+	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"fmt"
+	pb "github.com/hyperledger/fabric/protos/peer"
+)
+
+type FoozyCode struct {
+}
+
+func main() {
+	err := shim.Start(new(FoozyCode))
+	if err != nil {
+		fmt.Printf("Error starting chain code: %s", err)
+	}
+}
+
+func (fuzzy *FoozyCode) Init(stub shim.ChaincodeStubInterface) pb.Response {
+	return shim.Success(nil)
+}
+
+//peer的invoke和query都是调用此方法实现的
+func (fuzzy *FoozyCode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	//shim.Error产生异常响应，通知操作失败
+	//shim.Error("error test")
+	//shim.Success产生正常响应，通知操作成功
+	return shim.Success(nil)
+}
