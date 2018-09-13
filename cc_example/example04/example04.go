5c86dd35a1ec26e2f175f1120b3707a4aeace234
fengjj <fengjj@7zhilu.com>
2018-09-13 17:37:21 +0800

    增加private data collection的示例合约

--- cc_example/example04/example04.go
+++ cc_example/example04/example04.go
@@ -5,15 +5,10 @@ import (
 	"fmt"
 	pb "github.com/hyperledger/fabric/protos/peer"
 	"encoding/json"
+	"time"
 )
 
 type ExampleChainCode04 struct {
-}
-
-type Board struct {
-	Owner string `json:"owner"`
-	Color string `json:"color"`
-	Shape string `json:"shape"`
 }
 
 func main() {
@@ -31,70 +26,104 @@ func (code *ExampleChainCode04) Invoke(stub shim.ChaincodeStubInterface) pb.Resp
 	fun, args := stub.GetFunctionAndParameters()
 	fmt.Println(fun, args)
 	switch fun {
-	case "create":
-		return code.createBoard(stub, args)
-	case "getByOwner":
-		return code.getByOwner(stub, args)
+	case "put":
+		return code.put(stub, args)
+	case "get":
+		return code.get(stub, args)
+	case "history":
+		return code.history(stub, args)
+	case "delete":
+		return code.delete(stub, args)
+	case "getByRange":
+		return code.getByRange(stub, args)
 	default:
 		return shim.Error("unknown invoke function")
 	}
 	return shim.Error("unknown invoke function")
 }
-
-func (code *ExampleChainCode04) createBoard(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (code *ExampleChainCode04) put(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) < 3 {
 		return shim.Error("not enough args")
 	}
-	owner, color, shape := args[0], args[1], args[2]
-	//生成的复合key根据对象类型和属性列表用U+0000拼接成字符串作为key
-	key, err := stub.CreateCompositeKey("owner_color", []string{owner, color})
-	//预期输出U+0000owner_colorU+0000<owner>U+0000color
+	collection, key, value := args[0], args[1], args[2]
+	err := stub.PutPrivateData(collection, key, []byte(value))
 	if err != nil {
 		return shim.Error(err.Error())
 	}
-	board := new(Board)
-	board.Owner = owner
-	board.Color = color
-	board.Shape = shape
-	boardBytes, _ := json.Marshal(board)
-	err = stub.PutState(key, boardBytes)
-	return shim.Success([]byte(key))
+	return shim.Success(nil)
 }
 
-func (code *ExampleChainCode04) getByOwner(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+func (code *ExampleChainCode04) get(stub shim.ChaincodeStubInterface, args []string) pb.Response {
+	if len(args) < 2 {
+		return shim.Error("not enough args")
+	}
+	collection, key := args[0], args[1]
+	value, err := stub.GetPrivateData(collection, key)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(value)
+}
+
+func (code *ExampleChainCode04) history(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	if len(args) < 1 {
 		return shim.Error("not enough args")
 	}
-	owner := args[0]
-	//按对象类型和部分属性生成临时复合键，做前缀查询
-	result, err := stub.GetStateByPartialCompositeKey("owner_color", []string{owner})
+	key := args[0]
+	result, err := stub.GetHistoryForKey(key)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
 	defer result.Close()
-	boards := make([]map[string]interface{}, 0)
+	data := make([]map[string]interface{}, 0)
 	for result.HasNext() {
 		item, err := result.Next()
 		if err != nil {
 			return shim.Error(err.Error())
 		}
-		key := item.Key
-		//分割后的复合键，可以按属性顺序读取
-		objectType, args, err := stub.SplitCompositeKey(key)
+		data = append(data, map[string]interface{}{
+			"value":     string(item.Value),
+			"timestamp": time.Unix(item.Timestamp.Seconds, int64(item.Timestamp.Nanos)),
+			"is_delete": item.IsDelete,
+		})
+	}
+	dataBytes, _ := json.Marshal(data)
+	return shim.Success(dataBytes)
+}
+
+func (code *ExampleChainCode04) delete(stub shim.ChaincodeStubInterface, args [] string) pb.Response {
+	if len(args) < 2 {
+		return shim.Error("not enough args")
+	}
+	collection, key := args[0], args[1]
+	err := stub.DelPrivateData(collection, key)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	return shim.Success(nil)
+}
+
+func (code *ExampleChainCode04) getByRange(stub shim.ChaincodeStubInterface, args [] string) pb.Response {
+	if len(args) < 3 {
+		return shim.Error("not enough args")
+	}
+	collection, start, end := args[0], args[1], args[2]
+	result, err := stub.GetPrivateDataByRange(collection, start, end)
+	if err != nil {
+		return shim.Error(err.Error())
+	}
+	defer result.Close()
+	data := make([]map[string]interface{}, 0)
+	for result.HasNext() {
+		item, err := result.Next()
 		if err != nil {
 			return shim.Error(err.Error())
 		}
-		value := item.Value
-		board := new(Board)
-		json.Unmarshal(value, board)
-		data := map[string]interface{}{
-			"object_type": objectType,
-			"owner":       args[0],
-			"color":       args[1],
-			"shape":       board.Shape,
-		}
-		boards = append(boards, data)
+		data = append(data, map[string]interface{}{
+			"key":   item.Key,
+			"value": string(item.Value),
+		})
 	}
-	boardsBytes, _ := json.Marshal(boards)
-	return shim.Success(boardsBytes)
+	dataBytes, _ := json.Marshal(data)
+	return shim.Success(dataBytes)
 }
