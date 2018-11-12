1f909245725bbc22bc1e8857280920281e888843
fengjianjian <fengjj@7zhilu.com>
2018-11-12 10:54:23 +0800

    cryptogen使用修改

--- sm_usage/start.sh
+++ sm_usage/start.sh
@@ -8,34 +8,36 @@ generateArtifacts(){
     echo
     echo "##########################################################"
     echo "##### Generate certificates using cryptogen tool #########"
-	echo "##########################################################"
-	cryptogen generate --pluginPath ./smPlugin.so --config=./crypto-config.yaml
+    echo "##########################################################"
+	#using sm cryptogen or not
+    #cryptogensm generate --pluginPath ./smPlugin.so --config=./crypto-config.yaml
+	cryptogen generate --config=./crypto-config.yaml
 
-	echo "##########################################################"
-	echo "#########  Generating Orderer Genesis block ##############"
-	echo "##########################################################"
-	# Note: For some unknown reason (at least for now) the block file can't be
-	# named orderer.genesis.block or the orderer will fail to launch!
-	configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+    echo "##########################################################"
+    echo "#########  Generating Orderer Genesis block ##############"
+    echo "##########################################################"
+    # Note: For some unknown reason (at least for now) the block file can't be
+    # named orderer.genesis.block or the orderer will fail to launch!
+    configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
 
-	echo
-	echo "#################################################################"
-	echo "### Generating channel configuration  'mychannel.tx'          ###"
-	echo "#################################################################"
-	configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/mychannel.tx -channelID $CHANNEL_NAME
+    echo
+    echo "#################################################################"
+    echo "### Generating channel configuration  'mychannel.tx'          ###"
+    echo "#################################################################"
+    configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/mychannel.tx -channelID $CHANNEL_NAME
 
-	echo
-	echo "#################################################################"
-	echo "#######    Generating anchor peer update for Org1MSP   ##########"
-	echo "#################################################################"
-	configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+    echo
+    echo "#################################################################"
+    echo "#######    Generating anchor peer update for Org1MSP   ##########"
+    echo "#################################################################"
+    configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
 
-	echo
-	echo "#################################################################"
-	echo "#######    Generating anchor peer update for Org2MSP   ##########"
-	echo "#################################################################"
-	configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
-	echo
+    echo
+    echo "#################################################################"
+    echo "#######    Generating anchor peer update for Org2MSP   ##########"
+    echo "#################################################################"
+    configtxgen -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+    echo
 }
 
 generateArtifacts
